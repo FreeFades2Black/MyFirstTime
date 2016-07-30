591c72cdd95dfca757f1172089ac02873ff90562
FreeFades2Black <whall4.wh@gmail.com>
2016-07-30 09:44:05 -0500

    Add files via upload

---
+++ your.name2.py
@@ -0,0 +1,21 @@
+spam = 0
+while spam < 5:
+    print('Hello world!')
+    spam = spam + 1
+
+    name = ''
+
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+          break
+print ('I knew you would figure out your name. So, was it funny?')
+input()
+print ('Good')
+print ('Then you think I am funny?')
+input()
+
+    
+print('Thank you!')
+          
